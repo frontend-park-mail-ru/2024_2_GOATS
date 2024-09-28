8eea57e43877834a75782278b8b965f6d13e33d7
Ashurov Georgy <ashurov1308@mail.ru>
2024-09-28 22:32:40 +0300

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,12 @@ module.exports = {
     client: {
       overlay: false,
     },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
   },
   performance: {
     maxAssetSize: 500 * 1024,
