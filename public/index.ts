4f59db19a6c67b6b60d7e604dc14e8c7e7aa2aac
Ashurov Georgy <ashurov1308@mail.ru>
2024-10-25 19:22:34 +0300

    Update stores imports and webpack

--- public/index.ts
+++ public/index.ts
@@ -1,16 +1,15 @@
 import './index.scss';
-import { MainPage } from 'pages/MainPage/MainPage';
-import { initialStore } from 'store/InitialStore';
-import { userStore } from 'store/UserStore';
-import { authPageStore } from 'store/AuthPageStore';
-import { regPageStore } from 'store/RegPageStore';
-import { headerStore } from 'store/HeaderStore';
-import { mainPageStore } from 'store/MainPageStore';
-import { moviePageStore } from 'store/MoviePageStore';
-import { actorPageStore } from 'store/ActorPageStore';
-import { profilePageStore } from 'store/ProfilePageStore';
-import { roomPageStore } from 'store/RoomPageStore';
 import { router } from 'modules/Router';
+import { initialStore } from 'store/InitialStore';
+import 'store/UserStore';
+import 'store/AuthPageStore';
+import 'store/RegPageStore';
+import 'store/HeaderStore';
+import 'store/MainPageStore';
+import 'store/MoviePageStore';
+import 'store/ActorPageStore';
+import 'store/ProfilePageStore';
+import 'store/RoomPageStore';
 
 const root = document.getElementById('root') as HTMLElement;
 const pageElement = document.createElement('main');
@@ -24,19 +23,6 @@ root.appendChild(headerElement);
 root.appendChild(pageElement);
 root.appendChild(footerElement);
 root.appendChild(notifierElement);
-
-const mockFunction = () => {
-  console.log(initialStore);
-  console.log(userStore);
-  console.log(headerStore);
-  console.log(authPageStore);
-  console.log(regPageStore);
-  console.log(mainPageStore);
-  console.log(moviePageStore);
-  console.log(actorPageStore);
-  console.log(profilePageStore);
-  console.log(roomPageStore);
-};
 
 initialStore.start();
 router.start();
