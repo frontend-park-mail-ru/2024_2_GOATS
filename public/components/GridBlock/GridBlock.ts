51ba1133d7416d8af880f1dd123285d52ca6aa1f
Ashurov Georgy <ashurov1308@mail.ru>
2024-10-26 17:09:34 +0300

    Add redirecting to movie page from grid block

--- public/components/GridBlock/GridBlock.ts
+++ public/components/GridBlock/GridBlock.ts
@@ -5,24 +5,36 @@ export class GridBlock {
   #parent;
   #movies;
   #blockTitle;
+  #onImageClick;
 
-  constructor(parent: HTMLElement, movies: Movie[], blockTitle: string) {
+  constructor(
+    parent: HTMLElement,
+    movies: Movie[],
+    blockTitle: string,
+    onImageClick: (id: number) => void,
+  ) {
     this.#parent = parent;
     this.#movies = movies;
     this.#blockTitle = blockTitle;
+    this.#onImageClick = onImageClick;
   }
 
   render() {
     this.renderTemplate();
   }
 
-  /**
-   * get first three objects for displaying
-   * @param {}
-   * @returns {}
-   */
   getTop() {
     return this.#movies.slice(0, 3);
+  }
+
+  addImagesListeners() {
+    const images = this.#parent.querySelectorAll('.grid-block__element_image');
+    images.forEach((img) => {
+      img.addEventListener('click', () => {
+        const id = Number(img.id.split('image-')[1]);
+        this.#onImageClick(id);
+      });
+    });
   }
 
   renderTemplate() {
@@ -30,5 +42,7 @@ export class GridBlock {
       items: this.getTop(),
       title: this.#blockTitle,
     });
+
+    this.addImagesListeners();
   }
 }
