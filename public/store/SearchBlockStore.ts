c957c9b617b8d7c41f1f18c473823e897599bc0a
tkoibaev <tam.koib12@gmail.com>
2024-11-25 03:38:45 +0300

    Small fixes

--- public/store/SearchBlockStore.ts
+++ public/store/SearchBlockStore.ts
@@ -71,7 +71,7 @@ class SearchBlockStore {
       // });
 
       const response = await fetch(
-        `http://localhost:8080/api/movies/${this.#selectedNav}/search?query=${this.#searchValue}`,
+        `http://83.166.232.3:8080/api/movies/${this.#selectedNav}/search?query=${this.#searchValue}`,
         {
           method: 'GET',
           headers: {
