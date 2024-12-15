91b53bdf093db13c44e0ddcc12987eced19d20d2
Ashurov Georgy <ashurov1308@mail.ru>
2024-12-15 13:54:57 +0300

    Update to wss

--- public/store/RoomPageStore.ts
+++ public/store/RoomPageStore.ts
@@ -124,7 +124,7 @@ class RoomPageStore {
     this.#user = userStore.getUser();
     const ws = new WebSocket(
       // `ws://localhost:8080/api/room/join?room_id=${this.#roomIdFromUrl}&user_id=${this.#user.id}`,
-      `ws://cassette-world.ru/api/room/join?room_id=${this.#roomIdFromUrl}&user_id=${this.#user.id}`,
+      `wss://cassette-world.ru/api/room/join?room_id=${this.#roomIdFromUrl}&user_id=${this.#user.id}`,
     );
 
     this.#ws = ws;
